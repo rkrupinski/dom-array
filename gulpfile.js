79ac64366bf386668f9ee33fa1eca4e31d73e520
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-07-28 15:27:49 +0200

    Updated html parser

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var gulp = require('gulp')
 
 gulp.task('jshint', function () {
   return gulp
-      .src('lib/**/*.js')
+      .src('index.js')
       .pipe(jshint('.jshintrc'))
       .pipe(jshint.reporter('jshint-stylish'));
 });
--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 'use strict';
 
-module.exports = require('./lib/convert');
+var parse = require('parse-html')
+  , slice = [].slice;
+
+function convert(arg, ctx) {
+  var result = []
+    , query
+    , nodes;
+
+  switch (true) {
+    case (typeof arg === 'string'):
+      query = [];
+      nodes = [];
+
+      try {
+        query = (ctx || document).querySelectorAll(arg);
+      } catch (e) {
+        nodes = parse(arg);
+      }
+
+      query.length && (result = slice.call(query));
+      nodes.length && (result = nodes);
+      query = nodes = null;
+      break;
+    case (arg instanceof NodeList):
+      result = slice.call(arg);
+      break;
+    case (arg instanceof Element || arg === document ||
+        arg === window):
+      result.push(arg);
+      break;
+    case (Array.isArray(arg)):
+      result = arg;
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
+
+module.exports = convert;
